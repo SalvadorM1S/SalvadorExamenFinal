cd7060f7946235be47b8ee1d9ae5413fa285dde6
SalvadorM1S <111832173+SalvadorM1S@users.noreply.github.com>
2023-03-21 10:32:36 -0500

    Initial commit

---
+++ app/src/main/java/com/salvador/mariscal/salvadorexamenfinal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.salvador.mariscal.salvadorexamenfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
